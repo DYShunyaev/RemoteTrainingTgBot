14ebfebfbb576998dbc35e859c3dc560a9e22e8d
DYShunyaev <shunyaevd@gmail.com>
2025-10-19 20:42:08 +0300

    Initial commit

---
+++ src/main/java/d/shunyaev/RemoteTrainingTgBot/RemoteTrainingTgBotApplication.java
@@ -0,0 +1,13 @@
+package d.shunyaev.RemoteTrainingTgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTrainingTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTrainingTgBotApplication.class, args);
+	}
+
+}
